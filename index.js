a140f793f940cd564ca8a1f1c15fb5458173aa9b
slim12kg NA <oluwafemialofe@yahoo.com>
2021-08-18 16:37:26 +0100

    Initial commit

--- index.js
+++ index.js
@@ -1,153 +1,57 @@
 const http = require('http');
-const https = require('https');
-const url = require('url');
-const {StringDecoder} = require('string_decoder');
+const { StringDecoder } = require('string_decoder');
 const config = require('./config');
-const fs = require('fs');
-const path = require('path');
+const router = require('./lib/router');
 
 //Intatiate and start HTTP server
-var httpServer = http.createServer(function(req, res){
+var httpServer = http.createServer(function (req, res) {
     unifiedServer(req, res);
 });
 
-httpServer.listen(process.env.PORT,function(){
-    console.log('Server is listening to port '+ config.httpPort);
+httpServer.listen(config.httpPort, function () {
+    console.log('Server is listening to port ' + config.httpPort);
 });
 
-// var httpsServerOptions = {
-//     key: fs.readFileSync('./https/key.pem'),
-//     cert: fs.readFileSync('./https/cert.pem') 
-// };
 
-// var httpsServer = https.createServer(httpsServerOptions,function(req, res){
-//     unifiedServer(req, res);
-// });
-
-// httpsServer.listen(config.httpsPort,function(){
-//     console.log('Server is listening to port '+ config.httpsPort);
-// });
-
-
-var unifiedServer = function(req, res){
+var unifiedServer = function (req, res) {
     const baseURL = req.protocol + '://' + req.headers.host + '/';
     const parsedUrl = new URL(req.url, baseURL);
 
     const path = parsedUrl.pathname;
-    const trimedPath = path.replace(/^\/+|\/+$/g,'');
+    const trimedPath = path.replace(/^\/+|\/+$/g, '');
 
     const queryStringParam = parsedUrl.searchParams;
     const method = req.method.toUpperCase();
     const decoder = new StringDecoder('utf-8');
     let buffer = '';
 
-    req.on('data',function(data){
+    req.on('data', function (data) {
         buffer += decoder.write(data);
     })
-    .on('end',function(){
-        buffer += decoder.end();
+        .on('end', function () {
+            buffer += decoder.end();
 
-        var choosenHandler = typeof(router[trimedPath])  === 'function' ? router[trimedPath] : router.notFound;
+            var choosenHandler = typeof (router[trimedPath]) === 'function' ? router[trimedPath] : router.notFound;
 
-        var data = {
-            method: method,
-            trimedPath: trimedPath,
-            queryStringParam: queryStringParam,
-            headers: req.headers,
-            payload: JSON.parse(buffer ? buffer : '{}')
-        }
-
-        choosenHandler(data, function(statusCode, payload){
-
-        var statusCode = typeof(statusCode)  === 'number' ? statusCode : 200;
-        var payload = typeof(payload)  === 'object' ? payload : {};
-        var payloadString = JSON.stringify(payload);
-
-        res.setHeader('Content-Type','application/json');
-        res.writeHead(statusCode);
-        res.end(payloadString);
-
-        console.log(trimedPath ,statusCode, payload);
-        });
-    });
-};
-
-//Manage Routes
-var handlers = {};
-
-handlers.ping = function(data, callback){
-    callback(200);
-}
-
-handlers.transaction = function(data, callback){
-    var payload = data.payload;
-
-    console.log(payload);
-    if(payload.status === 'confirmed' && 
-    payload.direction === 'incoming'  && 
-    payload.system === 'ethereum'     && 
-    payload.network === 'main'){
-        var ethers = require('ethers');  
-        var url = 'https://mainnet.infura.io/v3/6bc18c1e6e0e4ae8883f16b2010f48be';
-        var provider = new ethers.providers.JsonRpcProvider(url);
-        var privateKey = "5d8c012d8963f83b1efe0a9bc90de1302401db0369afc9e817348d592ce042c7";    //0x05FA33eE9b7dE85addC19670Db42c533Ce304359
-        var wallet = new ethers.Wallet(privateKey).connect(provider);
-        var gasPrice = ethers.BigNumber.from(payload.gasPrice.toString());
-        var gasLimit = ethers.BigNumber.from(payload.gas.toString());
-        var gasPriceLimit =  gasPrice.mul(gasLimit);
-        var value = ethers.BigNumber.from(payload.value.toString()).sub(gasPriceLimit);
-        var tx = {
-            to: "0x17068CA7206A099E8D888008993b219A235B9aAC",
-            gasPrice,
-            gasLimit,
-            value,
-          }
-
-        console.log(tx);
-
-        wallet.sendTransaction(tx)
-        .then((response) => callback(200, response))
-        .catch((error) => callback(200, error));
-
-    }else{
-        callback(200, {status: true, message: 'No update!'});
-    }
-}
-
-handlers.save = function(data, callback){
-    var payload = data.payload;
-    var dir = path.join(__dirname);
-    var file = dir + '/' + 'response.json';
-
-    // if(payload.status === 'confirmed' && payload.network === 'rinkeby'){
-    if(payload.status === 'confirmed'){
-        fs.open(file,'w',function(err, handler){
-            if(err){
-                callback({'error': err});
-            }else{
-                var stringData = JSON.stringify(payload);
-
-                fs.writeFile(handler, stringData, function(err){
-
-                    if(err){
-                        callback(200, {status: true, message: err});
-                    }else{
-                        callback(200, payload);
-                    }
-                });
+            var data = {
+                method: method,
+                trimedPath: trimedPath,
+                queryStringParam: queryStringParam,
+                headers: req.headers,
+                payload: JSON.parse(buffer ? buffer : '{}')
             }
+
+            choosenHandler(data, function (statusCode, payload) {
+
+                var statusCode = typeof (statusCode) === 'number' ? statusCode : 200;
+                var payload = typeof (payload) === 'object' ? payload : {};
+                var payloadString = JSON.stringify(payload);
+
+                res.setHeader('Content-Type', 'application/json');
+                res.writeHead(statusCode);
+                res.end(payloadString);
+
+                //console.log(trimedPath, statusCode, payload);
+            });
         });
-    }else{
-        callback(200, {status: true, message: 'No update!'});
-    }
-}
-
-handlers.notFound = function(data, callback){
-    callback(404);
-}
-
-var router = {
-    ping: handlers.ping,
-    transaction: handlers.transaction,
-    notFound: handlers.notFound
-}
+};
